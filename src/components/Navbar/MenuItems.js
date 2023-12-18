245160d0f47dfb42eba680b9b18e4987e66b37c3
Iulia Alexandra Ignat <72752718+iuliaignat@users.noreply.github.com>
2023-12-18 18:16:14 +0200

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    clsName: 'nav-links',
+  },
+  {
+    title: 'Services',
+    url: '#',
+    clsName: 'nav-links',
+  },
+  {
+    title: 'Products',
+    url: '#',
+    clsName: 'nav-links',
+  },
+  {
+    title: 'Contact Us',
+    url: '#',
+    clsName: 'nav-links',
+  },
+  {
+    title: 'Sign up',
+    url: '#',
+    clsName: 'nav-links-mobile',
+  },
+];
